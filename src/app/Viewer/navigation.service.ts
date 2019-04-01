fb9fa9996d3f2a555148d2d71dcac9f279ad90c0
Joaolucasoliveira <joao.l.alves@hotmail.com>
2019-04-01 13:53:04 -0400

    1 file modified

--- src/app/Viewer/navigation.service.ts
+++ src/app/Viewer/navigation.service.ts
@@ -1,13 +1,35 @@
 import { Injectable } from '@angular/core'
+import { Page } from './page'
 
 @Injectable({
   providedIn: "root"
 })
-export class NavigationService{
+export class NavigationService {
 
-  constructor(){
+  pages: Page[] = [];
+
+  selectedPage
+  constructor() {
 
   }
 
-  
+  nextPage() {
+
+  }
+
+  previousPage() {
+
+  }
+
+  firstPage() {
+
+  }
+
+  lastPage() {
+
+  }
+
+  goToPage(pageNumber: number) {
+
+  }
 }
