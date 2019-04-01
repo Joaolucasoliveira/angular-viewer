f4bec90a65f9a82b18a8a62c0fb9f970e638a766
Joaolucasoliveira <joao.l.alves@hotmail.com>
2019-04-01 11:18:41 -0400

    1 file modified, 2 added

---
+++ src/app/NavigationBar/NavigationControls/navigation-controls.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: "app-viewer",
+  templateUrl: "./navigation-controls.component.html"
+})
+export class NavigationControls{
+  constructor(){
+
+  }
+}
