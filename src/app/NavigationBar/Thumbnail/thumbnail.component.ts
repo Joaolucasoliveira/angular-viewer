96fd0ad703eb70f02ffa990476e18994cc5018a6
Joaolucasoliveira <joao.l.alves@hotmail.com>
2019-04-01 12:00:37 -0400

    3 files modified

---
+++ src/app/NavigationBar/Thumbnail/thumbnail.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+
+@Component({
+  templateUrl: './thumbnail.component.html',
+  selector: 'app-thumbnail'
+})
+export class Thumbnail
+{
+  constructor(){
+
+  }
+}
