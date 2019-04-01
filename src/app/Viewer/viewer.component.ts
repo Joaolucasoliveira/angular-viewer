e6e5158b6936aa10e160d201769f65b8d7869ce5
Joaolucasoliveira <joao.l.alves@hotmail.com>
2019-04-01 13:33:03 -0400

    2 files modified

--- src/app/Viewer/viewer.component.ts
+++ src/app/Viewer/viewer.component.ts
@@ -1,12 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: "app-viewer",
   templateUrl: './viewer.component.html',
   styleUrls: ['./viewer.component.css']
 })
-export class ViewerComponent {
+export class ViewerComponent implements OnInit {
+  @ViewChild('canvasDoc') canvasRef: ElementRef;
+
   constructor() {
 
+  }
+  
+  ngOnInit(){
+    
   }
 }
