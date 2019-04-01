3cad4cd216650c310d5c80a6f5b8c0f8bd78b593
Joaolucasoliveira <joao.l.alves@hotmail.com>
2019-04-01 12:35:20 -0400

    4 files modified

--- src/app/NavigationBar/Thumbnail/thumbnail.component.ts
+++ src/app/NavigationBar/Thumbnail/thumbnail.component.ts
@@ -1,13 +1,18 @@
-import {Component} from '@angular/core'
+import { Component, Output, EventEmitter } from '@angular/core'
 
 @Component({
   templateUrl: './thumbnail.component.html',
   selector: 'app-thumbnail',
   styleUrls: ['./thumbnail.component.css']
 })
-export class Thumbnail
-{
-  constructor(){
+export class Thumbnail {
+  @Output() onClick: EventEmitter<any> = new EventEmitter();
 
+  constructor() {
+
+  }
+
+  handleClick() {
+    this.onClick.emit(null);
   }
 }
