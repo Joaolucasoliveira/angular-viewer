a09a8d445112bb896f0f88f45d3739684c277cdd
Joaolucasoliveira <joao.l.alves@hotmail.com>
2019-04-01 12:12:28 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavigationControls } from './NavigationBar/NavigationControls/navigation-controls.component';
+import { Thumbnail } from './NavigationBar/Thumbnail/thumbnail.component';
+import { NavigationBar } from './NavigationBar/navigation-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavigationControls],
+  declarations: [ AppComponent, HelloComponent, NavigationControls, Thumbnail, NavigationBar],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
