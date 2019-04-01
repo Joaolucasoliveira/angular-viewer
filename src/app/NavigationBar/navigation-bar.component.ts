a9a60819440dec401c68ac5b07351795b687d21e
Joaolucasoliveira <joao.l.alves@hotmail.com>
2019-04-01 11:53:48 -0400

    1 file modified, 3 added

---
+++ src/app/NavigationBar/navigation-bar.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'app-viewer-navBar',
+  templateUrl: './navigation-bar.component.html'
+})
+export class NavigationBar{
+  constructor(){
+
+  }
+}
